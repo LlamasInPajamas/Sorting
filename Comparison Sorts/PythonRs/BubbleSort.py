e122a26cfabb358be74a9af6b130dc4bfc477ec6
alpgc <alpgc@cin.ufpe.br>
2016-05-16 19:58:42 -0300

    BubblyPython

---
+++ Comparison Sorts/PythonRs/BubbleSort.py
@@ -0,0 +1,8 @@
+def BubbleSort(arr):
+	size = len(arr)
+	for i in reversed(range(1, size)):
+		for j in range(0, i):
+			if(arr[j] > arr[j + 1]):
+				swp=arr[j]
+				arr[j]=arr[j + 1]
+				arr[j + 1]=swp
