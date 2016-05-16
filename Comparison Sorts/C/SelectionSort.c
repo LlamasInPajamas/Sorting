81469dd4d8809ccddf553d8e52c2b2547904a2a4
alpgc <alpgc@cin.ufpe.br>
2016-05-16 19:03:36 -0300

    now a bit on C

---
+++ Comparison Sorts/C/SelectionSort.c
@@ -0,0 +1,20 @@
+void SelectionSort(int arr[],int size){
+	int minim;
+	int minimPos;
+	for(int i = 0; i < size ; i++){
+		minim = arr[i];
+		minimPos = i;
+		for(int j = i; j < size ; j++){
+			if(arr[j] < minim){
+				minim = arr[j];
+				minimPos = j;
+			}
+		}
+		arr[minimPos] = arr[i];
+		arr[i] = minim;
+	}
+}
+
+int main(){
+	
+}
