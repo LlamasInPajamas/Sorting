56168935695e9e2b3aaed693eb578a1e574d5693
alpgc <alpgc@cin.ufpe.br>
2016-05-16 18:56:35 -0300

    now a bit on C

---
+++ Comparison Sorts/C/InsertionSort.c
@@ -0,0 +1,17 @@
+void InsertionSort(int arr[],int size){
+	int swap;
+	for(int i = 0; i < size ; i++){
+		for(int j = i; j > 0 ; j--){
+			if(arr[j] > arr[j-1]){
+				swap = arr[j];
+				arr[j] = arr[j-1];
+				arr[j-1] = swap;
+			}else break;
+		}
+	}
+}
+
+int main(){
+	
+}
+
