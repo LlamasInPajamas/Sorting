458b4cff610b40468e1705eba1bbfa674e221dbe
alpgc <alpgc@cin.ufpe.br>
2016-05-17 12:32:57 -0300

    Renaming Python Folder+ QuickSort C++

---
+++ DivideNConquer/Cpp/QuickSort.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void QuickSort(int arr[],int start,int end){
+	if(start >= end)return ;
+	int pivot = start;
+	int i = start + 1;
+	int j = end;
+	while(i < j){
+		while(i < j && arr[i] <= arr[pivot])
+			i++;
+		while(j > i && arr[j] >= arr[pivot])
+			j--;
+		if(j > i)
+			swap(arr[j], arr[i]);
+	}
+	if(arr[j] < arr[pivot]){
+		swap(arr[j], arr[pivot]);
+		pivot = j;
+	}
+	QuickSort(arr, start, pivot - 1);
+	QuickSort(arr, pivot + 1, end);
+	return;
+}
+
+int main(){
+}
