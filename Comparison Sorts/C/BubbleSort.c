dc35968282f35d8194619bc339181e9fefda25fa
Arthurlpgc <arthurlpgcosta@gmail.com>
2016-05-18 09:27:28 -0300

    Update BubbleSort.c

--- Comparison Sorts/C/BubbleSort.c
+++ Comparison Sorts/C/BubbleSort.c
@@ -1,15 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void BubbleSort(int arr[],int size){
+void BubbleSort(int arr[],int size){ // we pass the array and the size of it as arguments
 	int swp;
-	for(int i = size - 1;i > 0; i--)
-		for(int j = 0;j < i; j++)
-			if(arr[j] > arr[j + 1]){
+	for(int i = size - 1;i > 0; i--){ // this for goes from the arrays end to the beginning
+		for(int j = 0; j < i; j++){ // this one goes from the begining to the to end defined by previous for
+			if(arr[j] > arr[j + 1]){ // now the idea is to compare every element with the next
+			// and swap them if the first one is more then the second
 				swp = arr[j];
 				arr[j] = arr[j + 1];
 				arr[j + 1] = swp;
 			}
+		}
+	}	
 }
 
 int main(){
