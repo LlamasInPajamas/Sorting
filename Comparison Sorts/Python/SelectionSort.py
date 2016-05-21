171b0379d871224ab80baa0c633e5c023ff872b6
Arthurlpgc <arthurlpgcosta@gmail.com>
2016-05-21 08:04:13 -0300

    Update SelectionSort.py

--- Comparison Sorts/Python/SelectionSort.py
+++ Comparison Sorts/Python/SelectionSort.py
@@ -1,11 +1,11 @@
 def SelectionSort( arr ):
 	size=len(arr)
-	for i in range(0, size):
-		minim = arr[i]
-		minimPos = i
-		for j in range(i, size):
-			if arr[j] < minim:
-				minim = arr[j]
+	for i in range(0, size):#changing the position of the change going from the first to the end
+		minim = arr[i]#resetting the minim
+		minimPos = i#resetting the position of the minim
+		for j in range(i, size):#rushing through variables to select which to change
+			if arr[j] < minim:#if find any better than the one we have now
+				minim = arr[j]#change it w the other
 				minimPos = j
-		arr[minimPos] = arr[i]
+		arr[minimPos] = arr[i]#swaaaap with the best match
 		arr[i] = minim
