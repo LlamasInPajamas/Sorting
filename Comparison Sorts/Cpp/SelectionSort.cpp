64b98a5827289192d212d467e4a98cfbaec17edc
Arthurlpgc <arthurlpgcosta@gmail.com>
2016-05-21 08:00:48 -0300

    Commenting

--- Comparison Sorts/Cpp/SelectionSort.cpp
+++ Comparison Sorts/Cpp/SelectionSort.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 
 void SelectionSort(int arr[],int size){
-	int minim;
-	int minimPos;
-	for(int i = 0; i < size ; i++){
-		minim = arr[i];
+	int minim;//variable to be used as minimum
+	int minimPos;//variable to use as the position of min
+	for(int i = 0; i < size ; i++){//changing the position of the change going from the first to the end
+		minim = arr[i];//resetting variables
 		minimPos = i;
-		for(int j = i; j < size ; j++){
-			if(arr[j] < minim){
-				minim = arr[j];
+		for(int j = i; j < size ; j++){//rushing through variables to select which to change
+			if(arr[j] < minim){//if find any better than the one we have now
+				minim = arr[j];//change it w the other
 				minimPos = j;
 			}
 		}
-		swap(arr[i], arr[minimPos]);
+		swap(arr[i], arr[minimPos]);//swaaap w the best match
 	}
 }
 
