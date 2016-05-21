7834455f7371f6dd4d7533767bf88e326f26ab2d
Arthurlpgc <arthurlpgcosta@gmail.com>
2016-05-21 07:58:54 -0300

    Commenting

--- Comparison Sorts/C/SelectionSort.c
+++ Comparison Sorts/C/SelectionSort.c
@@ -1,18 +1,18 @@
 #include <stdlib.h>
 
 void SelectionSort(int arr[],int size){
-	int minim;
-	int minimPos;
-	for(int i = 0; i < size ; i++){
-		minim = arr[i];
+	int minim;//variable to be used as minimum
+	int minimPos;//variable to use as the position of min
+	for(int i = 0; i < size ; i++){//changing the position of the change going from the first to the end
+		minim = arr[i];//resetting variables
 		minimPos = i;
-		for(int j = i; j < size ; j++){
-			if(arr[j] < minim){
-				minim = arr[j];
+		for(int j = i; j < size ; j++){//rushing through variables to select which to change
+			if(arr[j] < minim){//if find any better than the one we have now
+				minim = arr[j];//change it w the other
 				minimPos = j;
 			}
 		}
-		arr[minimPos] = arr[i];
+		arr[minimPos] = arr[i];//swaaap w the best match
 		arr[i] = minim;
 	}
 }
