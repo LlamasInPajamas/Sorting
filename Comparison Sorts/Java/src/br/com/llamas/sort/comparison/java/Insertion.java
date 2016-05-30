e0bdd55a0e83347d01f5bdf630d81b3ce8f383eb
Arthurlpgc <arthurlpgcosta@gmail.com>
2016-05-30 17:56:26 -0300

    Insertion was Inverted

--- Comparison Sorts/Java/src/br/com/llamas/sort/comparison/java/Insertion.java
+++ Comparison Sorts/Java/src/br/com/llamas/sort/comparison/java/Insertion.java
@@ -5,11 +5,11 @@ public class Insertion {
 	public static void sort(int array[], int size) {
 		for (int i = 0; i < size; i++) {
 			for (int j = i; j > 0; j--) {
-				if (array[j] > array[j - 1]) {
+				if (array[j] < array[j - 1]) {
 					int aux = array[j];
 					array[j] = array[j - 1];
 					array[j - 1] = aux;
-				}
+				}else break;
 			}
 		}
 	}
